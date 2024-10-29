b9161dd81520e0bf5d9967a1e31d749fd569688b
GaelFG17 <gaelfg1720@gmail.com>
2024-10-28 22:51:15 -0600

    when

---
+++ src/when.kt
@@ -0,0 +1,10 @@
+fun main() {
+    val x = 2
+
+    when (x) {
+        1 -> println("x es 1")
+        2 -> println("x es 2")
+        3 -> println("x es 3")
+        else -> println("x no es 1, 2, ni 3")
+    }
+}
